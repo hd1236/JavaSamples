08e4c646f8d9cab2f60ba036ae0eae057906c7d0
hand <hand@cloudream.com>
2019-03-23 11:17:29 +0800

    Initial commit

---
+++ app/src/main/java/com/hand/javasamples/ThreadAlternatePrint/AlternateMultiThread.java
@@ -0,0 +1,53 @@
+package com.hand.javasamples.ThreadAlternatePrint;
+
+/**
+ * 三个及以上线程轮流打印
+ */
+public class AlternateMultiThread extends Thread {
+
+    private int mType;
+    private int mTotalType;
+    private Object mLock;
+    public static int sIndex;
+    public static int sCurrentType;
+
+
+    public AlternateMultiThread(String name, int type, int totalType, Object lock) {
+        this.setName(name);
+        mType = type;
+        mLock = lock;
+        mTotalType = totalType;
+    }
+
+    @Override
+    public void run() {
+
+        while (sIndex <= 100) {
+
+            synchronized (mLock) {
+
+                //判断type
+                if (sCurrentType == mType) {
+
+                    //打印index，并+1
+                    System.out.println(getName() + ":" + sIndex);
+                    sIndex++;
+
+                    //处理下一个打印线程type
+                    sCurrentType = (mType + 1) % mTotalType;
+                    //唤醒其他线程
+                    mLock.notifyAll();
+                }
+
+                try {
+                    if (sIndex <= 100) {
+                        mLock.wait();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+}
